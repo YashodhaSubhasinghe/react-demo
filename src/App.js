b82e3979cb5da0eb3b595c3708bc8e91061ff7f0
Yashodha <yashodasubasinghe19@gmail.com>
2022-09-17 01:08:59 +0530

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route, Links, Link } from 'react-router-dom';
 import './App.css';
+import CreateUser from './components/CreateUser';
+import EditUser from './components/EditUser copy';
+import ListUser from './components/ListUser';
 
 function App() {
   return (
     <div className="App">
-      <h1>helloo</h1>
+      <h1>React CRUD using PHP API and MySQL</h1>
 
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">List Users</Link>
+            </li>
+
+            <li>
+              <Link to="user/create">Create Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route index element={<ListUser />} />
+          <Route path='user/create' element={<CreateUser />} />
+          <Route path='user/:id/edit' element={<EditUser />} />
+        </Routes>
+
+      </BrowserRouter>
     </div>
   );
 }
