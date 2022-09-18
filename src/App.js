cc2ec3887694222b35c7fd5dcba05bde33a0a951
Yashodha <yashodasubasinghe19@gmail.com>
2022-09-18 21:05:37 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import { BrowserRouter, Routes, Route, Links, Link } from 'react-router-dom';
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 import './App.css';
 import CreateUser from './components/CreateUser';
-import EditUser from './components/EditUser copy';
+import EditUser from './components/EditUser';
 import ListUser from './components/ListUser';
 
 function App() {
   return (
     <div className="App">
-      <h1>React CRUD using PHP API and MySQL</h1>
+      <h5>React CRUD operations using PHP API and MySQL</h5>
 
       <BrowserRouter>
         <nav>
@@ -15,19 +15,16 @@ function App() {
             <li>
               <Link to="/">List Users</Link>
             </li>
-
             <li>
-              <Link to="user/create">Create Users</Link>
+              <Link to="user/create">Create User</Link>
             </li>
           </ul>
         </nav>
-
         <Routes>
           <Route index element={<ListUser />} />
-          <Route path='user/create' element={<CreateUser />} />
-          <Route path='user/:id/edit' element={<EditUser />} />
+          <Route path="user/create" element={<CreateUser />} />
+          <Route path="user/:id/edit" element={<EditUser />} />
         </Routes>
-
       </BrowserRouter>
     </div>
   );
--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -1,73 +1,63 @@
 import { useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-export default function CreateUser() {
+export default function ListUser() {
+    const navigate = useNavigate();
 
-    //The React useState Hook allows us to track state in a function component. 
-    //State generally refers to data or properties that need to be tracking in an application.
-    const [inputs, setInputs] = useState({})
+    const [inputs, setInputs] = useState([]);
 
-    //handleChange function - handleChange is triggered by the input element and triggers the changing of the this. state. value property which in turn changes the value of the input field. 
-    //This is important because react uses states to display information in the DOM.
     const handleChange = (event) => {
         const name = event.target.name;
         const value = event.target.value;
-        setInputs(values => ({ ...values, [name]: value })); //... will create a single one
-
+        setInputs(values => ({...values, [name]: value}));
     }
-
-    //handleSubmit function 
     const handleSubmit = (event) => {
-        event.preventDefault(); //preventDefault() method cancels the event if it is cancelable, meaning that the default action that belongs to the event will not occur.
+        event.preventDefault();
 
-        axios.post('http://localhost:8080/api/users/save' , inputs);
-        console.log(inputs);
+        axios.post('http://localhost:8080/api/user/save', inputs).then(function(response){
+            console.log(response.data);
+            navigate('/');
+        });
+        
     }
-
     return (
         <div>
-            <h1>Create Users</h1>
-
+            <h1>Create user</h1>
             <form onSubmit={handleSubmit}>
                 <table cellSpacing="10">
                     <tbody>
                         <tr>
                             <th>
-                                <label>Name</label>
+                                <label>Name: </label>
                             </th>
                             <td>
                                 <input type="text" name="name" onChange={handleChange} />
                             </td>
                         </tr>
-
                         <tr>
                             <th>
-                                <label>Email</label>
+                                <label>Email: </label>
                             </th>
-                            <td>
+                            <td> 
                                 <input type="text" name="email" onChange={handleChange} />
                             </td>
                         </tr>
-
                         <tr>
                             <th>
-                                <label>Mobile</label>
+                                <label>Mobile: </label>
                             </th>
                             <td>
                                 <input type="text" name="mobile" onChange={handleChange} />
                             </td>
                         </tr>
-
-                        <td colSpan="2" align="right">
-                            <button>Save</button>
-                        </td>
-
-
-
+                        <tr>
+                            <td colSpan="2" align ="right">
+                                <button>Save</button>
+                            </td>
+                        </tr>
                     </tbody>
                 </table>
-
-
             </form>
         </div>
     )
